3dee270f957e4b7bdfd8996ed6a600a2c982d4f6
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-16 08:10:03 -0400

    connected mogose in serverjs

---
+++ server.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(express.static("public"));
+
+app.use(require("./routes"));
+//connecting mongose///
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/AJ-Social-Network",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    iseFindAndModify: false,
+  }
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`listening on localhost:${PORT}`));
