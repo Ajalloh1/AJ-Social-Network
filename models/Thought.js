0f0b7cfede15baf74fa1b25d087956687857e0cd
Ajalloh1 <mys4jalloh@gmail.com>
2022-10-18 12:28:38 -0400

    added in thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,50 +1,28 @@
 const { Schema, model, Types } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
 
-const messageSchema = new Schema(
-    {
-      messageText: {
-        type: String,
-        required: [
-          true,
-          'You must enter a message between 1 - 250 characters.',
-        ],
-        maxlength: 250,
-        minlength: 1,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (date) => {
-          return new Intl.DateFormat('en-US', {
-            dateStyle: 'full',
-            timeStyle: 'short',
-          }).format(date);
-        },
-      },
-    username: {
-        type: String,
-        required: [true, 'You must provide a username.'],
-      },
-      reactions: [reactionSchema],
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-      toJSON: {
-        getters: true,
-        virtuals: true,
-      },
-      id: false,
-    }
-  );
 
+    reactionBody: {
+      type: String,
+      maxlength: 280,
+      required: true,
+    },
 
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
 
-  //getting all reaction message and responses//
-
-  messageSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
-  });
-  
-  const Thought = model('thought', messageSchema);
-  
-  module.exports = Thought;
+    username: {
+      type: String,
+      required: true,
+    },
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
